39b89301547e3d54fdf9f6a1615863864ae6feb6
djaldim <diego@jettax.com.br>
2022-02-01 10:08:53 -0300

    Create transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,7 @@ class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Expenses',
       home: MyHomePage()
     );
   }
@@ -17,14 +19,43 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
 
+  final _transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'Novo Tenis de Corrida', 
+      value: 310.76, 
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Conta de Luz', 
+      value: 260.99, 
+      date: DateTime.now()
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Center(
-        child: Text('Versão incial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          )
+        ],
       ),
     );
   }
